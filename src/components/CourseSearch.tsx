aa23d0caf6a5973678dab7923f5d84c2fd971f68
KoiXS1 <166782933+KoiXS1@users.noreply.github.com>
2025-04-09 13:02:56 -0400

    Added the CSS for the typical course search page screen.

--- src/components/CourseSearch.tsx
+++ src/components/CourseSearch.tsx
@@ -1,4 +1,9 @@
 "use client";
+import { useState, useEffect } from "react";
+import CourseSearchArea from "./courseSearchComponents/CourseSearchArea";
+import OverviewBox from "./courseSearchComponents/OverviewBox";
+import ResourcesBox from "./courseSearchComponents/ResourcesBox";
+import { MagnifyingGlass } from "@phosphor-icons/react";
 
 const CourseSearch: React.FC = () => {
   // TODO: Add state management for:
@@ -13,27 +18,53 @@ const CourseSearch: React.FC = () => {
   // TODO: Implement OpenAI API integration
   // TODO: Implement MongoDB integration
 
+  //STATE MANAGEMENT
+  const [courseSearchData, setCourseSearchData] = useState({
+    prefix: "-",
+    courseNum: 0,
+    courseName: "",
+  });
+
+  const [activeTab, setActiveTab] = useState();
+
+  const [courseData, setCourseData] = useState({
+    title: "-",
+    topics: [],
+    description: "-",
+    successPlan: "-",
+  });
+
   return (
-    <div>
-      {/* TODO: Add search bar */}
-      <div>
-        <input type="text" placeholder="Course Prefix" />
-        <input type="text" placeholder="Course Number" />
-        <input type="text" placeholder="Course Name" />
-        <button>Search</button>
+    <div className="w-3/5 m-auto">
+      {/*TITLE*/}
+      <div className="flex justify-center items-center">
+        <MagnifyingGlass size={64} color="#6CFEFE" className="ml-[10px] mr-[10px] drop-shadow-[0_0_10px_rgba(108,254,254,1)]"/>
+        <h2 className="text-[3em] text-[#6CFEFE] ml-[10px] mr-[10px] drop-shadow-[0_0_10px_rgba(108,254,254,1)]" >Course Search</h2>
       </div>
 
+      {/* SEARCH AREA */}
+      <h4 className="text-[#D163D7] m-[10px]">Enter the 4 letter prefix, 4 digit number, and course name.</h4>
+      <CourseSearchArea />
+
       {/* TODO: Add tabs */}
-      <div>
-        <button>Overview</button>
-        <button>Resources</button>
+      <div className="">
+        <button className="m-[10px] p-[5px] pl-[15px] pr-[15px] text-white border border-[3px] border-white rounded-[10px] inline">
+          Overview
+        </button>
+        <button className="m-[10px] p-[5px] pl-[15px] pr-[15px] text-white border border-[3px] border-white rounded-[10px] inline">
+          Resources
+        </button> 
       </div>
 
       {/* TODO: Add course overview content */}
-      <div>{/* Course details will go here */}</div>
+      <div>
+        <OverviewBox />
+      </div>
 
       {/* TODO: Add resources content */}
-      <div>{/* Resources will go here */}</div>
+      <div>
+        <ResourcesBox />
+      </div>
 
       {/* TODO: Add sidebar */}
       <div>
