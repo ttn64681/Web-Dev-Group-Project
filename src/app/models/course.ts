d329361e849b843b2a7a285e4768db81c48fc07a
KoiXS1 <166782933+KoiXS1@users.noreply.github.com>
2025-04-16 02:33:38 -0400

    Feature: Added mongoose models.

---
+++ src/app/models/course.ts
@@ -0,0 +1,54 @@
+import mongoose, {Schema, Document, Model } from "mongoose";
+
+interface CourseTemplate extends Document {
+    prefix: string;
+    number: number;
+    title: string;
+    description: string;
+    topics: string[]
+    resources: {
+        aiLinks: string[],
+        postIds: string[]
+    }
+}
+
+const courseSchema = new Schema<CourseTemplate>({
+    prefix: {
+        type: String,
+        required: true
+    },
+    number: {
+        type: Number,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    topics: {
+        type: [String],
+        required: true
+    },
+    resources: {
+        type:  {
+            aiLinks: {
+                type: [String],
+                required: true
+            },
+            postIds: {
+                type: [String],
+                required: true
+            }
+        },
+        required: true
+    },
+});
+
+const Course: Model<CourseTemplate> = 
+    mongoose.models.Post || mongoose.model<CourseTemplate>("Course", courseSchema);
+
+export default Course;
