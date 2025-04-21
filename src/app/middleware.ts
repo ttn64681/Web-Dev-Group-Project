00231f50f59653b0d2c3f274162e039f786f272d
Andrew Ingraham <andrew.ingraham555@gmail.com>
2025-04-21 17:46:23 -0400

    Update middleware.ts

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -3,17 +3,18 @@ import { authConfig } from "./auth.config";
 import NextAuth from "next-auth";
 
 const { auth } = NextAuth(authConfig);
-
 const middleware = async (request: NextRequest) => {
     const { pathname } = request.nextUrl;
     const session = await auth();
     const isAuthenticated = !!session?.user;
-    console.log(isAuthenticated, pathname);
 
     const publicPaths = ["/", "/login",];
 
+    //searchParams passes information to the page being redirected to
     if (!isAuthenticated && !publicPaths.includes(pathname)) {
-        return NextResponse.redirect(new URL("/", request.url));
+        const loginUrl = new URL('/login', request.url);
+        loginUrl.searchParams.set('reason', 'not_logged_in');
+        return NextResponse.redirect(loginUrl);
     }
 
     return NextResponse.next();
@@ -22,7 +23,6 @@ const middleware = async (request: NextRequest) => {
 export const config = {
     matcher: [
         "/contribute/:path*",
-        "/course-search/:path*",
     ],
 };
 
